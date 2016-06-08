2dcf4936db1e8cebc13342659cb9b257131e2c9e
Vipin Khushu <vipinkhushu@hotmail.com>
2016-06-08 23:05:49 +0530

    New conent updation

--- query.php
+++ query.php
@@ -1,8 +1,21 @@
 <?php
-$to="hk131997@gmail.com";
-$subject="manan query";
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="b7_18237466_mananqueryinfo";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$firstname=$_POST['firstname'];
+$email=$_POST['email'];
 $msg=$_POST['msg'];
-mail($to,$subject,$msg);
+ if(isset($firstname) && isset($email) && isset($msg) )
+    {
+    	$queryInsert = "INSERT INTO querytable VALUES('','$firstname','$email','$msg')";
+		$queryInsertResult = $conn->query($queryInsert);
+    }
+
 header('Location:index.html');
 ?>
 
